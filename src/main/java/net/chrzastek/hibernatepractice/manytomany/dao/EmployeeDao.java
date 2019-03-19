e706a6c390e08bdee40a372401ff8da648cfdb00
Wojciech Chrzastek <w.chrzastek@gmail.com>
2019-03-19 15:23:14 +0100

    Add own named queries with parameters

--- src/main/java/net/chrzastek/hibernatepractice/manytomany/dao/EmployeeDao.java
+++ src/main/java/net/chrzastek/hibernatepractice/manytomany/dao/EmployeeDao.java
@@ -1,6 +1,6 @@
 package net.chrzastek.hibernatepractice.manytomany.dao;
 
-import net.chrzastek.hibernatepractice.manytomany.Company;
+import net.chrzastek.hibernatepractice.manytomany.Employee;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Transactional
 @Repository
-public interface CompanyDao extends CrudRepository<Company, Integer> {
+public interface EmployeeDao extends CrudRepository<Employee, Integer> {
   @Query(nativeQuery = true)
-  List<Company> retrieveCompaniesWithGivenName(@Param("PARAMETER") String parameter);
+  List<Employee> retrieveEmployeesWithGivenName(@Param("LASTNAME") String lastname);
 }
